767b46317a3bf382df2f0d8e93ab9034c32b7639
Carolina <carolin861@gmail.com>
2018-02-08 15:26:28 -0500

    My Django Girls app, first commit

---
+++ myvenvdjangogirls/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/carolinalondono/Documents/djangogirls/myvenvdjangogirls/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
